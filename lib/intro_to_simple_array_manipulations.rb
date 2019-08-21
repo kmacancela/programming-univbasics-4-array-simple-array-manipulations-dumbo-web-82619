444e2132a02171892de6634a16c2b1f33d242737
Karina Macancela <karymacancela@gmail.com>
2019-08-21 16:08:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,15 @@ end
 def using_uniq(array)
   array.uniq
 end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete(index)
+end
